9e577c2a1fd1b2e337e371ff4e87c10d0817a825
niacdoial <liam.marsh.home@gmail.com>
2016-12-11 13:18:06 +0100

    the texture update part 2
    therefor adapt the autoloader

--- automated-converter/IOmanager.py
+++ automated-converter/IOmanager.py
@@ -12,13 +12,13 @@ bpy.data.objects.remove(bpy.data.objects['Camera'],True)
 bpy.data.cameras.remove(bpy.data.cameras['Camera'],True)
 
 try:
-    bpy.ops.blemd.importer(filepath=path)
-except AttributeError:  # module not here
+    bpy.ops.blemd.importer(filepath=path, imtype='AS_PNG')
+except AttributeError:  # module not loaded: do it manually
     import blemd
     temp = blemd.BModel()
     temp.SetBmdViewExePath('C:\\Users\\Liam\\Bureau\\MaxBMD-multi-texcoords\\')
-    temp.Import(path, 5, True, False, True, True, 'XFILE', False, False)
+    temp.Import(path, 5, True, False, True, True, 'XFILE', False, 'AS_PNG', False)
 
 bpy.ops.export_scene.fbx(filepath=path[:-3]+'fbx', axis_forward='Y', axis_up='Z')
 
-bpy.ops.wm.quit_blender()
+bpy.ops.wm.quit_blender()  # quit blender the clean way
